d064be4009336da7c0dafffa5d04bacdb4c3b09b
nefrrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2025-11-08 12:01:23 +0530

    Create captureRoutes.js

---
+++ backend/routes/captureRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { captureContent } from "../controllers/captureController.js";
+const router = express.Router();
+
+router.post("/", captureContent);
+export default router;
